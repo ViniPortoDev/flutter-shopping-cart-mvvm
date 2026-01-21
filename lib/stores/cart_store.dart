0391b3ce71cd747592b12d4e5859cf9ccef2a6bc
Vinicius Porto <vinicius.p.a.corin@gmail.com>
2026-01-21 09:06:57 -0300

    feat: implement cart screen and store

--- lib/stores/cart_store.dart
+++ lib/stores/cart_store.dart
@@ -1,23 +1,18 @@
 import 'package:flutter/foundation.dart';
+
 import '../domain/entities/cart.dart';
 import '../domain/entities/product.dart';
 
 class CartStore extends ChangeNotifier {
   Cart _cart = Cart();
+  bool isLoading = false;
+  String? error;
 
   Cart get cart => _cart;
 
-  int get totalItems => _cart.totalItems;
-  double get subtotal => _cart.subtotal;
-  bool contains(Product product) => _cart.contains(product);
-
   void add(Product product) {
-    try {
-      _cart = _cart.add(product);
-      notifyListeners();
-    } catch (e) {
-      rethrow;
-    }
+    _cart = _cart.add(product);
+    notifyListeners();
   }
 
   void decrement(Product product) {
@@ -25,8 +20,29 @@ class CartStore extends ChangeNotifier {
     notifyListeners();
   }
 
+  Future<bool> checkout() async {
+    isLoading = true;
+    error = null;
+    notifyListeners();
+
+    await Future.delayed(const Duration(seconds: 2));
+
+    final success = DateTime.now().millisecondsSinceEpoch % 2 == 0;
+
+    if (!success) {
+      error = 'Falha ao processar pagamento';
+      isLoading = false;
+      notifyListeners();
+      return false;
+    }
+
+    isLoading = false;
+    notifyListeners();
+    return true;
+  }
+
   void clear() {
-    _cart = _cart.clear();
+    _cart = Cart();
     notifyListeners();
   }
 }
